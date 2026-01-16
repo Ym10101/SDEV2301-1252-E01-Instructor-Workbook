ec0b3191afbe15fd43b69e81119acc74bffe61cb
Sam Wu <swu@nait.ca>
2026-01-15 20:31:38 -0700

    Added Lesson 4 CupConverter

---
+++ SDEV2301_Module1/L04_CupConverter/Program.cs
@@ -0,0 +1,28 @@
+﻿// This program converts cups to fluid ounces
+// Get the numbers of cups
+
+
+double cups = GetCups();
+double ounces = CupsToOunces(cups);
+DisplayResults(cups, ounces);
+
+double CupsToOunces(double cups)
+{
+    // Formula: 1 cup = 8 ounces
+    return cups * 8.0;
+}
+
+void DisplayResults(double cups, double ounces)
+{
+    Console.WriteLine($"{cups} cups equals {ounces} fluid ounces");
+}
+
+double GetCups()
+{
+    // Prompt the user for the number of cups
+    Console.WriteLine("This program converts measurements in cups to fluid ounces.");
+    Console.WriteLine("For your reference the formula is:");
+    Console.WriteLine("\t1 cup = 8 fluid ounces.");
+    Console.WriteLine("\nEnter the number of cups.");
+    return double.Parse(Console.ReadLine());
+}
