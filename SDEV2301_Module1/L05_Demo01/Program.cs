d6c19afe38f9cea52bf61f2f8ca151c5efda5b5b
Sam Wu <swu@nait.ca>
2026-01-22 19:55:14 -0700

    Added demo1 for Lesson 5a

---
+++ SDEV2301_Module1/L05_Demo01/Program.cs
@@ -0,0 +1,6 @@
+﻿using L05_Demo01;   // the Student class namespace 
+
+Student currentStudent = new Student();
+currentStudent.Name = "Alex";
+currentStudent.Age = 21;
+currentStudent.PrintInfo();
---
+++ SDEV2301_Module1/L05_Demo01/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L05_Demo01
+{
+    public class Student
+    {
+        public string Name;
+        public int Age;
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{Name} is {Age} years old.");
+        }
+    }
+}
